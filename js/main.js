f5cb08ffc82bd8ba330988831951d8ecb35d36d7
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2024-03-12 12:43:04 +0400

    the adaption of the eight section is compelted

--- js/main.js
+++ js/main.js
@@ -66,14 +66,43 @@ if (accordionIcons && accordionTexts) {
 //   });
 // });
 
-// ________________________________ swiper _____________________________
-const swiper = new Swiper('.main__fifth-quote', {
+// ________________________________ sixth section swiper _____________________________
+new Swiper('.main__fifth-quote', {
   loop: true,
 
   navigation: {
     nextEl: '.main__fifth-buttons-container-arrow-right',
     prevEl: '.main__fifth-buttons-container-arrow-left',
   },
+
+});
+// ________________________________ eight section swiper _____________________________
+// Initialize the second Swiper instance
+new Swiper('.main__eight-our-blog-list-container', {
+
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  spaceBetween: 15, // px
+  slidesPerView: 3,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.main__eight-next-button',
+    prevEl: '.main__eight-prev-button',
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    550: {
+      slidesPerView: 2,
+    },
+    800: {
+      slidesPerView: 3,
+    }
+  }
 
 });
 // ________________________________ burger _____________________________
