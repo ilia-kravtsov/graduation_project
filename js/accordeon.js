bbea671a89686ccb76fc0bc737df3f070e818309
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2024-03-23 16:23:45 +0400

    accordeon fixing

--- js/accordeon.js
+++ js/accordeon.js
@@ -1,41 +1,26 @@
-// ____________________________ аккордеон __________________________________________________________________
-// ____________________________ один открыт, остальные закрываются _________________________________________
-// Выбираю все иконки
-const accordionIcons = document.querySelectorAll('.main__sixth-right-list-item-icon');
-
-// Выбираю все кнопки
 const accordionButtonsIcons = document.querySelectorAll('.main__sixth-right-list-item-button');
-
-// Выбираю все скрывающиеся параграфы
 const accordionTexts = document.querySelectorAll('.main__sixth-right-list-item-text');
-if (accordionButtonsIcons && accordionTexts) {
-  // Прохожу циклом по каждой кнопке и добавляю обработчик
-  accordionButtonsIcons.forEach((button, index) => {
-    // Добавляю слушатель события на каждую кнопку
-    button.addEventListener('click', function(e) {
+const accordionOpenLines = document.querySelectorAll('.main__sixth-right-list-item-open-line');
+
+if (accordionOpenLines && accordionTexts) {
+  accordionOpenLines.forEach((openLine, index) => {
+    openLine.addEventListener('click', function(e) {
       e.preventDefault();
 
-      // Нахожу ближайший родительский элемент
-      const listItem = e.target.closest('.main__sixth-right-list-item');
+      const button = openLine.querySelector('.main__sixth-right-list-item-button');
+      const textContent = openLine.closest('.main__sixth-right-list-item').querySelector('.main__sixth-right-list-item-text');
 
-      // Нахожу текст в этом родительском элементе
-      const textContent = listItem.querySelector('.main__sixth-right-list-item-text');
-
-      // Проверяю открыт ли уже какой-либо другой аккордeон
       const isOpen = textContent.classList.contains('--accordion-active');
 
-      // Закрываю все аккордeоны
       accordionTexts.forEach(text => {
         text.classList.remove('--accordion-active');
       });
 
-      // Закрываю все иконки
       accordionButtonsIcons.forEach(button => {
         const icon = button.querySelector('.main__sixth-right-list-item-icon');
         icon.classList.remove('--accordion-icon-active');
       });
 
-      // Открываю текущий аккордеон если он был закрыт
       if (!isOpen) {
         textContent.classList.add('--accordion-active');
         const icon = button.querySelector('.main__sixth-right-list-item-icon');
@@ -48,28 +33,3 @@ if (accordionButtonsIcons && accordionTexts) {
 
 
 
-
-// ____________________________ один открыт, остальные не закрываются _________________________________________
-
-// // Select all accordion control icons
-// const accordionIcons = document.querySelectorAll('.main__sixth-right-list-item-icon');
-
-// // Loop through each accordion control icon
-// accordionIcons.forEach(icon => {
-//   // Add click event listener to each icon
-//   icon.addEventListener('click', function(e) {
-//     e.preventDefault();
-
-//     // Find the closest parent list item
-//     const listItem = e.target.closest('.main__sixth-right-list-item');
-
-//     // Find the text content of the list item
-//     const textContent = listItem.querySelector('.main__sixth-right-list-item-text');
-
-//     // Toggle the active class on the text content
-//     textContent.classList.toggle('--accordion-active');
-
-//     // Toggle the active class on the icon
-//     icon.classList.toggle('--accordion-icon-active');
-//   });
-// });
